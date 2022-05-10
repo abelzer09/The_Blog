6b99e6446b658da2c798e33994e64f2e02d7d06d
abelzer09 <aaronrbelzer@gmail.com>
2022-05-10 10:52:25 -0500

    adde get route for comment.js, fixed errors with index.js and userdata.json files

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
-const { Post, User, Comment } = require('../models');
-const withAuth = require('../utils/auth');
+const { Post, User, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
-    
+router.get('/:postid', withAuth, async (req, res) => {
+    try {
+        const commentInfo = await Post.findByPk(req.params.postid, {
+            include: [
+                {
+                    model: Comment
+                }
+            ]
+        }
+         )
+         console.log(commentInfo);
+        // await commentInfo.update({description: req.body.description})
+        // res.json(postInfo)
+    } catch (err) {
+        res.status(400).json(err)
+    }
 })
+
+module.exports = router;
